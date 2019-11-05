43eb7345cee66bce2c50b49abf9da0ba0089fe72
totoblabla3 <stalkersergej@gmail.com>
2019-11-05 19:22:02 +0300

    Updates

---
+++ precache-manifest.c680c583bf29100f4449e063808f604d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "264f1413fb2d588154be5b842624b0db",
+    "url": "/snakeGame/index.html"
+  },
+  {
+    "revision": "847208516ab419e13082",
+    "url": "/snakeGame/static/css/main.e81e0d6a.chunk.css"
+  },
+  {
+    "revision": "14a941af04484f0229f5",
+    "url": "/snakeGame/static/js/2.947729e6.chunk.js"
+  },
+  {
+    "revision": "847208516ab419e13082",
+    "url": "/snakeGame/static/js/main.896961b2.chunk.js"
+  },
+  {
+    "revision": "c1099d0ddd4696c8024f",
+    "url": "/snakeGame/static/js/runtime-main.60e5a7e3.js"
+  }
+]);
