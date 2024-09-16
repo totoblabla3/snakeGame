b4efc684d7f8832b434903da845762659dcc3a09
totoblabla3 <totoblabla3@users.noreply.github.com>
2024-09-16 03:25:50 +0000

    deploy: 957e25c28cd9867e911fda78567e3db006616b8b

--- precache-manifest.0c62fcb5d7c328e35145417761ce61e3.js
+++ precache-manifest.0c62fcb5d7c328e35145417761ce61e3.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "5ff02b52238b56798f5914c3df944b7b",
+    "revision": "40d6c867e3c7f76dea3980aa7d2b91a3",
     "url": "/snakeGame/index.html"
   },
   {
-    "revision": "76606c1ab5e8ded5beec",
+    "revision": "e4bc4db80effc9c854c8",
     "url": "/snakeGame/static/css/main.30630342.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/snakeGame/static/js/2.024f7d40.chunk.js"
   },
   {
-    "revision": "76606c1ab5e8ded5beec",
-    "url": "/snakeGame/static/js/main.e12d3305.chunk.js"
+    "revision": "e4bc4db80effc9c854c8",
+    "url": "/snakeGame/static/js/main.00bf567d.chunk.js"
   },
   {
     "revision": "c1099d0ddd4696c8024f",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/snakeGame/precache-manifest.d50ec18dc4ea380c5a5f3b2dba81e54d.js"
+  "/snakeGame/precache-manifest.0c62fcb5d7c328e35145417761ce61e3.js"
 );
 
 self.addEventListener('message', (event) => {
